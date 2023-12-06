2ac431c157c072771885b550e7e77eeaab3becf2
Hernan Aymard <haymard@haymard.altostrat.com>
2023-12-06 11:53:37 +0000

    Inicial

---
+++ src/shippingservice/main.go
@@ -0,0 +1,231 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+
+	"cloud.google.com/go/profiler"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
+
+	pb "github.com/GoogleCloudPlatform/microservices-demo/src/shippingservice/genproto"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
+
+	"cloud.google.com/go/firestore"
+	"google.golang.org/api/compute/v1"
+        "golang.org/x/oauth2/google"
+)
+
+const (
+	defaultPort = "50051"
+)
+
+var log *logrus.Logger
+
+func init() {
+	log = logrus.New()
+	log.Level = logrus.DebugLevel
+	log.Formatter = &logrus.JSONFormatter{
+		FieldMap: logrus.FieldMap{
+			logrus.FieldKeyTime:  "timestamp",
+			logrus.FieldKeyLevel: "severity",
+			logrus.FieldKeyMsg:   "message",
+		},
+		TimestampFormat: time.RFC3339Nano,
+	}
+	log.Out = os.Stdout
+}
+
+func main() {
+	if os.Getenv("DISABLE_TRACING") == "" {
+		log.Info("Tracing enabled, but temporarily unavailable")
+		log.Info("See https://github.com/GoogleCloudPlatform/microservices-demo/issues/422 for more info.")
+		go initTracing()
+	} else {
+		log.Info("Tracing disabled.")
+	}
+
+	if os.Getenv("DISABLE_PROFILER") == "" {
+		log.Info("Profiling enabled.")
+		go initProfiling("shippingservice", "1.0.0")
+	} else {
+		log.Info("Profiling disabled.")
+	}
+
+	port := defaultPort
+	if value, ok := os.LookupEnv("PORT"); ok {
+		port = value
+	}
+	port = fmt.Sprintf(":%s", port)
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	var srv *grpc.Server
+	if os.Getenv("DISABLE_STATS") == "" {
+		log.Info("Stats enabled, but temporarily unavailable")
+		srv = grpc.NewServer()
+	} else {
+		log.Info("Stats disabled.")
+		srv = grpc.NewServer()
+	}
+	svc := &server{}
+	pb.RegisterShippingServiceServer(srv, svc)
+	healthpb.RegisterHealthServer(srv, svc)
+	log.Infof("Shipping Service listening on port %s", port)
+
+	// Register reflection service on gRPC server.
+	reflection.Register(srv)
+	if err := srv.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+// server controls RPC service responses.
+type server struct{}
+
+// Check is for health checking.
+func (s *server) Check(ctx context.Context, req *healthpb.HealthCheckRequest) (*healthpb.HealthCheckResponse, error) {
+	return &healthpb.HealthCheckResponse{Status: healthpb.HealthCheckResponse_SERVING}, nil
+}
+
+func (s *server) Watch(req *healthpb.HealthCheckRequest, ws healthpb.Health_WatchServer) error {
+	return status.Errorf(codes.Unimplemented, "health check via Watch not implemented")
+}
+
+// GetQuote produces a shipping quote (cost) in USD.
+func (s *server) GetQuote(ctx context.Context, in *pb.GetQuoteRequest) (*pb.GetQuoteResponse, error) {
+	log.Info("[GetQuote] received request")
+	defer log.Info("[GetQuote] completed request")
+
+	// 1. Generate a quote based on the total number of items to be shipped.
+	quote := CreateQuoteFromCount(0)
+
+	// 2. Generate a response.
+	return &pb.GetQuoteResponse{
+		CostUsd: &pb.Money{
+			CurrencyCode: "USD",
+			Units:        int64(quote.Dollars),
+			Nanos:        int32(quote.Cents * 10000000)},
+	}, nil
+
+}
+
+// ShipOrder mocks that the requested items will be shipped.
+// It supplies a tracking ID for notional lookup of shipment delivery status.
+func (s *server) ShipOrder(ctx context.Context, in *pb.ShipOrderRequest) (*pb.ShipOrderResponse, error) {
+	log.Info("[ShipOrder] received request")
+	defer log.Info("[ShipOrder] completed request")
+
+	// haymard - Me conecto a Firestore
+	context := context.Background() 
+        credentials, err := google.FindDefaultCredentials(ctx,compute.ComputeScope)
+	if err != nil {
+		log.Fatalln(err)
+        }
+	client, err := firestore.NewClient(context, credentials.ProjectID)
+	if err != nil {
+		log.Fatalln(err)
+	  }
+	
+	// 1. Create a Tracking ID
+	baseAddress := fmt.Sprintf("%s, %s, %s", in.Address.StreetAddress, in.Address.City, in.Address.State)
+	id := CreateTrackingId(baseAddress)
+
+		// haymard - Genero Shipping
+	envios := client.Collection("shipments")
+	orden := envios.Doc(id)
+	
+	type CartItem struct {
+		ProductId            string   `protobuf:"bytes,1,opt,name=product_id,json=productId,proto3" json:"product_id,omitempty"`
+		Quantity             int32    `protobuf:"varint,2,opt,name=quantity,proto3" json:"quantity,omitempty"`
+	}
+
+	type Envio struct {
+		id string 
+		StreetAddress        string   
+	    City                 string   
+	    State                string   
+	    Country              string   
+	    ZipCode              int32
+	    Items                []CartItem 
+	}
+	
+	listaItems := make([]CartItem, len(in.Items))
+	for i := 0; i < len(in.Items); i++ {
+        listaItems[i] = CartItem{ ProductId: in.Items[i].ProductId, Quantity : in.Items[i].Quantity}
+    }
+ 
+	wr, err := orden.Create(context, Envio{id: id, 
+		                                   StreetAddress: in.Address.StreetAddress, 
+										   City: in.Address.City, 
+										   State: in.Address.State,
+										   Country: in.Address.Country,
+										   ZipCode: in.Address.ZipCode,
+										   Items: listaItems})
+	if err != nil {
+		// TODO: Handle error.
+		log.Error(err)
+	}
+	log.Info("Envio realizado ")
+	fmt.Println(wr)
+
+	defer client.Close()
+
+	// 2. Generate a response.
+	return &pb.ShipOrderResponse{
+		TrackingId: id,
+	}, nil
+}
+
+func initStats() {
+	//TODO(arbrown) Implement OpenTelemetry stats
+}
+
+func initTracing() {
+	// TODO(arbrown) Implement OpenTelemetry tracing
+}
+
+func initProfiling(service, version string) {
+	// TODO(ahmetb) this method is duplicated in other microservices using Go
+	// since they are not sharing packages.
+	for i := 1; i <= 3; i++ {
+		if err := profiler.Start(profiler.Config{
+			Service:        service,
+			ServiceVersion: version,
+			// ProjectID must be set if not running on GCP.
+			// ProjectID: "my-project",
+		}); err != nil {
+			log.Warnf("failed to start profiler: %+v", err)
+		} else {
+			log.Info("started Stackdriver profiler")
+			return
+		}
+		d := time.Second * 10 * time.Duration(i)
+		log.Infof("sleeping %v to retry initializing Stackdriver profiler", d)
+		time.Sleep(d)
+	}
+	log.Warn("could not initialize Stackdriver profiler after retrying, giving up")
+}
